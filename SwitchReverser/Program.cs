f6f8e5c9f9fe03297f6b42e13cede0e9b2a9ceed
Kristian Gergov <kristian.gergov1@gmail.com>
2022-06-28 00:38:07 +0300

    Quick fix.

--- SwitchReverser/Program.cs
+++ SwitchReverser/Program.cs
@@ -8,9 +8,12 @@ namespace SwitchReverser
         static void Main(string[] args)
         {
             // This program is retarded
-            var yes = File.ReadAllLines("../../../NewFile1.txt");
-            var newString = yes.Select(line => line.Split("=>")).Aggregate("", (current, split) => current + $"{split[1].Trim().Replace(',', (char) 0)} => {split[0].Trim()},\n");
-            File.WriteAllText("./out.txt", newString);
+            for (var i = 0; i < args.Length; i++)
+            {
+                var yes = File.ReadAllLines(args[i]);
+                var newString = yes.Select(line => line.Split("=>")).Aggregate("", (current, split) => current + $"{split[1].Trim().Replace(',', (char) 0)} => {split[0].Trim()},\n");
+                File.WriteAllText($"./out{i}.txt", newString);
+            }
         }
     }
 }
