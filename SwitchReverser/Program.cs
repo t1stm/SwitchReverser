96f7f7a0f497d734225132375ebd204e7f3b23d2
Kristian Gergov <kristian.gergov1@gmail.com>
2022-06-28 00:36:51 +0300

    Initial Push

---
+++ SwitchReverser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace SwitchReverser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This program is retarded
+            var yes = File.ReadAllLines("../../../NewFile1.txt");
+            var newString = yes.Select(line => line.Split("=>")).Aggregate("", (current, split) => current + $"{split[1].Trim().Replace(',', (char) 0)} => {split[0].Trim()},\n");
+            File.WriteAllText("./out.txt", newString);
+        }
+    }
+}
